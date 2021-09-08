ae702501a3894b17301c5bd7eb8bf6be51f1e506
marcosfretas <marcosafjm@gmail.com>
2021-09-08 19:02:55 -0300

    continuação

--- curriculo_flutter/lib/Screens/Home/curriculo.dart
+++ curriculo_flutter/lib/Screens/Home/curriculo.dart
@@ -1,5 +1,7 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 
 class Curriculo extends StatelessWidget {
@@ -8,12 +10,12 @@ class Curriculo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Currículo"),),
-      body:Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 150,),
-              ClipRRect(
+      appBar: AppBar(title: const Text("Currículo")),
+      body: Column(
+        children: [
+          const SizedBox(height: 150,),
+          Center(
+            child:ClipRRect(
               borderRadius: BorderRadius.circular(190),
               child: Image.asset(
                 "assets/images/marcos.jpg",
@@ -22,9 +24,55 @@ class Curriculo extends StatelessWidget {
                 width: 150,
               )
             ),
-            ],
           ),
+          const SizedBox(height: 30),
+          const Center(
+            child: Text(
+              "Marcos Antonio de Freitas Júnior",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            )
+          ),
+          const SizedBox(height: 5),
+          const Center(
+            child: Text(
+              "Elias Alves da Cunha, 278",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            )
+          ),
+          const Center(
+            child: Text(
+              "38747-060 | Cidade Jardim | Patrocínio | MG",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            )
+          ),
+          const Center(
+            child: Text(
+              "(34) 9 9956-4396",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            )
+          ),
+          const Center(
+            child: Text(
+              "marcosafjm@gmail.com",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            )
+          ),
+          const Center(
+            child: Text(
+              "20 anos | Brasileiro | Solteiro",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            )
+          ),
+          const Divider(
+          color: Colors.black,
+          height: 20,
+          thickness: 1,
+          indent: 10,
+          endIndent: 10,
         ),
+        
+        ],
+      ),
     );
   }
 }
