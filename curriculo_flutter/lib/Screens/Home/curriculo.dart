f3bb236e0739c7a463971950ef0bd0199f8c07f8
marcosfretas <marcosafjm@gmail.com>
2021-09-09 20:40:22 -0300

    semifinal

--- curriculo_flutter/lib/Screens/Home/curriculo.dart
+++ curriculo_flutter/lib/Screens/Home/curriculo.dart
@@ -5,13 +5,14 @@ import 'package:flutter/painting.dart';
 
 
 class Curriculo extends StatelessWidget {
+
   const Curriculo({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Currículo")),
-      body: Column(
+      body: ListView(
         children: [
           const SizedBox(height: 150,),
           Center(
@@ -63,17 +64,141 @@ class Curriculo extends StatelessWidget {
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
             )
           ),
+          const SizedBox(height: 10),
           const Divider(
-          color: Colors.black,
-          height: 20,
-          thickness: 1,
-          indent: 10,
-          endIndent: 10,
-        ),
-        
+            color: Colors.black,
+            height: 20,
+            thickness: 1,
+            indent: 10,
+            endIndent: 10,
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            "OBJETIVO: Em busca de um emprego, me coloco à disposição da empresa para aprimorar as habilidades da função e assim contribuir para os resultados da organização.",
+            style: TextStyle(fontSize: 18),
+            textAlign: TextAlign.left,
+          ),
+          const SizedBox(height: 40,),
+          const Text(
+            "RESUMO DE QUALIFICAÇÕES",
+            style: TextStyle(fontWeight: FontWeight.w900, fontSize: 25, decoration: TextDecoration.underline),
+            textAlign: TextAlign.left,
+          ),
+          const SizedBox(height: 15),
+          const ListTile(
+            leading:  MyBullet(),
+            title: Text('Conhecimento na área administrativa, com habilidade para atuar no auxílio em rotinas do setor, contato com clientes, organização do ambiente de trabalho e nas demais atividades diárias de uma empresa, visando à qualidade do serviço prestado.', style: TextStyle(fontSize: 18)),
+          ),
+          const ListTile(
+            leading:  MyBullet(),
+            title: Text('Possuo nível básico de inglês e conhecimento em uso dos programas do pacote Office da Microsoft.', style: TextStyle(fontSize: 18)),
+          ),
+          const ListTile(
+            leading:  MyBullet(),
+            title: Text('Possuo nível básico de inglês e conhecimento em uso dos programas do pacote Office da Microsoft.', style: TextStyle(fontSize: 18)),
+          ),
+          const ListTile(
+            leading:  MyBullet(),
+            title: Text('Conhecimento em HTML, CSS, PHP e WordPress.', style: TextStyle(fontSize: 18)),
+          ),
+          const ListTile(
+            leading:  MyBullet(),
+            title: Text('Auxílio no desenvolvimento de três websites feitos em WordPress: www.soleluaimobiliaria.com.br, www.clickassistencia.come atualmente estou desenvolvimento minha loja que se chama godslipper.com.br.', style: TextStyle(fontSize: 18)),
+          ),
+          const SizedBox(height: 40,),
+          const Text(
+            "FORMAÇÃO ACADÊMICA",
+            style: TextStyle(fontWeight: FontWeight.w900, fontSize: 25, decoration: TextDecoration.underline),
+            textAlign: TextAlign.left,
+          ),
+          const ListTile(
+            leading:  MyBullet(),
+            title: Text('Cursando o quarto período de Sistema de Informação na UNIPAM.', style: TextStyle(fontSize: 18)),
+          ),
+          const Text(
+            "IDIOMA",
+            style: TextStyle(fontWeight: FontWeight.w900, fontSize: 25, decoration: TextDecoration.underline),
+            textAlign: TextAlign.left,
+          ),
+          const ListTile(
+            leading:  MyBullet(),
+            title: Text('Inglês – Básico', style: TextStyle(fontSize: 18)),
+          ),
+          const Text(
+            "INFORMÁTICA",
+            style: TextStyle(fontWeight: FontWeight.w900, fontSize: 25, decoration: TextDecoration.underline),
+            textAlign: TextAlign.left,
+          ),
+          const ListTile(
+            leading:  MyBullet(),
+            title: Text('Operador de Microcomputador Microsoft Office Word – Básico', style: TextStyle(fontSize: 18)),
+          ),
+          const ListTile(
+            leading:  MyBullet(),
+            title: Text('Operador de Microcomputador Microsoft Office Excel – Básico', style: TextStyle(fontSize: 18)),
+          ),
+          const ListTile(
+            leading:  MyBullet(),
+            title: Text('Operador de Microcomputador Microsoft Office PowerPoint – Básico', style: TextStyle(fontSize: 18)),
+          ),
+          const Text(
+            "CURSOS",
+            style: TextStyle(fontWeight: FontWeight.w900, fontSize: 25, decoration: TextDecoration.underline),
+            textAlign: TextAlign.left,
+          ),
+          const ListTile(
+            leading:  MyBullet(),
+            title: Text('12/2019 Aprendizagem Industrial de Processos Administrativos - SENAI, Jovem Aprendiz da TEBRA - Construção e Montagem LTDA. ', style: TextStyle(fontSize: 18)),
+          ),
+          const Center(
+            child: Text(
+              "",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            )
+          ),
+          const SizedBox(height: 10),
+          const Divider(
+            color: Colors.black,
+            height: 20,
+            thickness: 1,
+            indent: 10,
+            endIndent: 10,
+          ),
         ],
       ),
     );
   }
 }
+
+
+class MyBullet extends StatelessWidget{
+
+  const MyBullet({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      height: 10.0,
+      width: 10.0,
+      decoration:  const BoxDecoration(
+        color: Colors.black,
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+}
+
+/* class Date extends StatelessWidget{
+
+  const Date({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    DateTime now = DateTime.now();
+    return Text(
+      "Patrocínio " ${now.day}, "|" ${now.month}, "-" ${now.year},  
+    );
+  }
+} */
 
